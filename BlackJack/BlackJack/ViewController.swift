993afa15714eda8af90ba167e9b1d925255db471
Era <era@172-16-215-155.DYNAPOOL.NYU.EDU>
2015-02-15 18:44:31 -0500

    created project

---
+++ BlackJack/BlackJack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackJack
+//
+//  Created by Era on 2/15/15.
+//  Copyright (c) 2015 Era Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
